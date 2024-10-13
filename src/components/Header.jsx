b55270a444ecd602935a2d94e1ba7199c25a46f5
Feras Alkilani <eng.feras90@gmail.com>
2024-10-13 21:39:10 +0200

    create the navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,13 @@
-import React from "react";
 import MenuItem from "./MenuItem";
-import DarkModeSwitch from "./DarkModeSwitch";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
-    <div className="flex justify-between mx-2 mx-w-6xl sm:mx-auto items-center py-6">
-      <div className="flex  ">
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
+      <div className="flex">
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
@@ -17,7 +16,7 @@ export default function Header() {
         <Link href="/">
           <h2 className="text-2xl">
             <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
-              IMDB
+              IMDb
             </span>
             <span className="text-xl hidden sm:inline">Clone</span>
           </h2>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import NavbarItem from "./NavbarItem";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-center items-center space-x-4  dark:bg-gray-600 bg-amber-100 lg:text-lg p-4 ">
+      <NavbarItem title="Trending" param="fetchTrending" />
+      <NavbarItem title="Top Rated" param="fetchTopRated" />
+    </div>
+  );
 }
