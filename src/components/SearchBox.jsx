292c84be01444062ddc868592f6e20b923e0f848
Feras Alkilani <eng.feras90@gmail.com>
2024-10-20 11:12:08 +0200

    add search ability

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,5 +1,37 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function SearchBox() {
-  return <div>SearchBox</div>;
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!search) return;
+    router.push(`/search/${search}`);
+  }
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex max-w-6xl mx-auto justify-between items-center px-5 "
+    >
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        type="text"
+        placeholder="Search keywords..."
+        className="w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1"
+      />
+
+      <button
+        disabled={!search}
+        typeof="submit"
+        className="text-amber-600 disabled:text-gray-400"
+      >
+        Search
+      </button>
+    </form>
+  );
 }
