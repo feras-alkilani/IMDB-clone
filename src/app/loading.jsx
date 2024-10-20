bea9a2d749f4dde6b19e4391a6b2e81af7b0545d
Feras Alkilani <65820163+feras-alkilani@users.noreply.github.com>
2024-10-20 15:05:33 +0200

    Update loading.jsx

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 
-export default function loading() {
+export default function Loading() {
   return (
-    <div className="flex justify-center">
-      <Image className="h-96" src="spinner.svg" alt="loading..." />
+    <div className="flex justify-center items-center h-screen">
+      <Image 
+        className="h-24 w-24" 
+        src="/spinner.svg" // تأكد من استخدام / في بداية المسار 
+        alt="Loading..." 
+        width={96}
+        height={96} 
+      />
     </div>
   );
 }
